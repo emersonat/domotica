8fb762b8cb8f91994c9aa3a0a193f6955553974e
Emerson <emersonat@ig.com.br>
2012-11-26 23:59:06 -0200

    configurando spring securiry

--- src/main/java/br/com/emersondeandrade/modelo/core/usuario/Role.java
+++ src/main/java/br/com/emersondeandrade/modelo/core/usuario/Role.java
@@ -7,8 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
+import org.springframework.jmx.export.annotation.ManagedOperationParameter;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
@@ -20,7 +23,7 @@ public class Role implements Serializable, GrantedAuthority{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@SequenceGenerator(name = "sequenceGenerator", sequenceName = "role_seq", allocationSize = 1)
+	@SequenceGenerator(name = "sequenceGenerator", sequenceName = "public.role_id_role_seq", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.AUTO, generator = "sequenceGenerator")
 	@Column(name = "id_role", unique = true, nullable = false)
 	private int id;
@@ -28,7 +31,9 @@ public class Role implements Serializable, GrantedAuthority{
 	@Column(nullable = false,length = 30)
 	private String nome;
 	
-	
+	@ManyToOne
+	@JoinColumn(name="id_usuario",nullable = false)
+	private Usuario usuario;
 	
 	public int getId() {
 		return id;
@@ -45,6 +50,12 @@ public class Role implements Serializable, GrantedAuthority{
 	
 	public String getAuthority() {
 		return getNome();
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	
